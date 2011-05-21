62687690f235783fe7bbf9bf351a08aaa3b7efbc
Alex Nano <alexnano@ucla.edu>
2011-05-20 18:05:20 -0700

    correct analyze.py

--- web/uag/analyze.py
+++ web/uag/analyze.py
@@ -5,25 +5,21 @@ import commands
 import json
 import config as cfg
 
-# in future this should be run from seperate thread via cron that only operates on
-# uploaded images that have not yet been analyzed
-# ie
-# db.getAllUnanalyzed()
+import sys
+sys.path.append("/var/www/html/lighting/app/process")
+import process
+
+def histogram(id, mode):
+	imageFileName = cfg.imageFilePath+data.getFilenameFrom(id);
+	aName = "histogram_"+str(mode)
+	data.insertAnalysisWithID(id, aName, json.dumps(process.imgprocess(imageFileName, mode)))
+	return data.getEntryFromID(id)
 	
-
-# meanwhile, since it's just histogram:
-
-
-# perform analysis on media
-
-def histogram(id):
+	
+	'''	
 	imageFileName = cfg.imageFilePath+data.getFilenameFrom(id);
 	result = commands.getoutput("convert "+imageFileName+" histogram:- | identify -format %c -")
 	aName = "histogram"
 	data.insertAnalysisWithID(id, aName, json.dumps(result));
 	return data.getEntryFromID(id)
-	
-	#note for pattern generation:
-	#json.loads(result) to reconsistute newline (or just explode on \\n)
- 
-
+	'''
