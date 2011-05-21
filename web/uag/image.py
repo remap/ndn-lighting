c338cb7383450727876aa843109bfb77fc7cc990
Alex Nano <alexnano@ucla.edu>
2011-05-21 11:27:06 -0700

    image module

---
+++ web/uag/image.py
@@ -0,0 +1,29 @@
+import database as data
+import subprocess
+import os
+import commands
+import json
+import config as cfg
+
+# this is a utility module for processing image (at moment system calls to imagemagick)
+
+def histogram(id):
+	imageFileName = cfg.imageFilePath+data.getFilenameFrom(id);
+	result = commands.getoutput("convert "+imageFileName+" histogram:- | identify -format %c -")
+	aName = "histogram"
+	data.insertAnalysisWithID(id, aName, json.dumps(result));
+	return data.getEntryFromID(id)
+	
+	#note for pattern generation:
+	#json.loads(result) to reconsistute newline (or just explode on \\n)
+ 
+
+def resize(id):
+	imageFileName = cfg.imageFilePath+data.getFilenameFrom(id);
+	result = commands.getoutput("convert "+imageFileName+" -resize 640x "+imageFileName)
+	return data.getEntryFromID(id)
+	
+	#note for pattern generation:
+	#json.loads(result) to reconsistute newline (or just explode on \\n)
+ 
+
