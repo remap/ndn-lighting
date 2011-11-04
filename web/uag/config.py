3d0e2c0b901c8d9e106dba13a444fbd9a920ff2a
Alex Nano <alexnano@ucla.edu>
2011-11-03 21:13:47 -0700

    updated readme

--- web/uag/config.py
+++ web/uag/config.py
@@ -24,6 +24,18 @@ interestPrefix = "/ucla.edu/apps/lighting/TV1/fixture/"
 # seperate prefix for colorflex (PARC Demo)
 flexPrefix = "/ucla.edu/apps/lighting/fixture/"
 
+controlHost = "ssh lighting@borges.metwi.ucla.edu"
+# ssh command (on TV1 gumstix)
+startSequencerCommand = "/home/lighting/ndn-lighting/scripts/webStartLightingTV1"
+stopSequencerCommand = "/home/lighting/ndn-lighting/scripts/webStopLightingTV1"
+	
+
+
+#FOLLOWING IS *DEPRICATED*
+#
+#LEAVING FOR REFERENCE
+
+
 # NDN node & user with signedLightInterest command
 # (sequencer was running as www-data from bigriver)
 # SSH was used for parc demo... don't need anymore, but leaving for emergency / fallback
@@ -31,26 +43,10 @@ lightHost = "ssh nano@borges.metwi.ucla.edu"
 
 # full path to interest executable on host
 
-	# ssh (path on borges)
+# ssh (path on borges)
 signedInterestCommand = "/home/nano/ndn-lighting/ndn/signedInterest/signedNDNClient"
 
 
-controlHost = "ssh lighting@remap.is-very-good.org"
-# ssh command (on TV1 gumstix)
-startSequencerCommand = "/home/lighting/ndn-lighting/scripts/webStartLightingTV1"
-stopSequencerCommand = "/home/lighting/ndn-lighting/scripts/webStopLightingTV1"
-	
 # local interest command:
 #signedInterestCommand = "/home/lighting/ndn-lighting/ndn/signedInterest/signedNDNClient"
 
-#temporary translation until finalized / moved elsewhere
-names = {
-"living-room-front":"ColorBlast/1",
-"living-room-right":"ColorBlast/2",
-"entrance-door":"ColorBlast/3",
-"window-right":"ColorBlast/4",
-"stairs":"ColorBlaze1/1",
-"bedroom":"ColorBlaze2/1",
-"kitchen":"ColorBlast/1",
-"window-left":"ColorBlast/4",
-"incandescent":"ArtNet"}
